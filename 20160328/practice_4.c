6fef3fa546e17c394502ca060a1a1d6f2b25fc78
ETzzz <shuntys92@gmail.com>
2016-03-28 15:31:53 +0800

    updated

--- 20160328/practice_4.c
+++ 20160328/practice_4.c
@@ -3,5 +3,23 @@
 
 int main()
 {
-
+	int num, max = 0, count = 0;
+	char ch;
+	do
+	{
+		count = 0;
+		printf("請輸入任一正整數：\n");
+		scanf("%d", &num);
+		while (num > 0)
+		{
+			count++;
+			if (max <= num % 10)
+				max = num % 10;
+			num = num / 10;
+		}
+		printf("此數為%d位數，最大數字是：%d\n", count, max);
+		fflush(stdin);
+		printf("請輸入y繼續或其他字母離開。\n");
+		scanf("%c", &ch);;
+	} while (ch == 'y');
 }
