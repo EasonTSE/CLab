6a9ee4681cc9ef2a1cc07caf93284bfa2fc47511
Eason Tse <easontse@users.noreply.github.com>
2016-04-04 08:26:07 +0800

    HW3

---
+++ HW3/HW3_B10409029_1.c
@@ -0,0 +1,51 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+int main()
+{
+	int total_row;
+
+	do
+	{
+		printf("請輸入列數：");
+		scanf("%d", &total_row);
+	} while (total_row % 2 == 0);
+
+	int row,space,star,median;
+
+	median = (total_row + 1) / 2;
+
+	int counter = median,rStar = 1;
+	for (row = 1; row <= median; row++)
+	{
+		for (space = median - row; space >= 1; space--)
+			printf(" ");
+		for (star = 1; star <= row + median - counter; star++)
+			printf("*");
+		printf("\n");
+		counter--;
+	}
+	for (row = median + 1; row <= total_row; row++)
+	{
+		for (space = row - median; space >= 1; space--)
+			printf(" ");
+		for (star = total_row - (rStar * 2); star >= 1; star--)
+			printf("*");
+		printf("\n");
+		rStar++;
+	}
+
+	printf("請輸入列數：");
+	scanf("%d", &total_row);
+
+	for (row = 1; row <= total_row; row++)
+	{
+		for (space = total_row - row; space >= 1; space--)
+			printf(" ");
+		for (star = 1; star <= row; star++)
+			printf("*");
+		printf("\n");
+	}
+
+	return 0;
+}
