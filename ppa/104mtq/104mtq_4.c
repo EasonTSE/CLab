932b78e4a730b1d890146ed1ba2f64da67259434
Eason Tse <eason.tse@easontse.me>
2016-04-25 17:41:37 +0800

    104mtq

--- ppa/104mtq/104mtq_4.c
+++ ppa/104mtq/104mtq_4.c
@@ -25,29 +25,18 @@ int payment(int in_hr, int in_min, float mile, int t_min)
 {
 	int start = in_hr * 60 + in_min + 3;
 	int end = start + t_min - 3;
-//  printf("start = %d\n", start);
-//  printf("end = %d\n", end);
+
 	float e_mile = mile - 1.5;
 	int base_fee = 75;
-	int distance_fee = e_mile * 2 * 5;
+	float distance_fee = e_mile * 2 * 5;
 
 	int i, time_fee = 0;
 	for (i = start + 1; i <= end; i++)
 	{
 		if ((i > 8 * 60 && i <= 10 * 60) || (i > 17 * 60 && i <= 19 * 60))
-    {
-//      printf("in: %d\n",i);
 			time_fee += 7;
-    }
 		else
-    {
-//      printf("out: %d\n", i);
 			time_fee += 5;
-    }
 	}
-
-//  printf("base_fee = %d\n", base_fee);
-//  printf("distance_fee = %d\n", distance_fee);
-//  printf("time_fee = %d\n", time_fee);
 	return base_fee + distance_fee + time_fee;
 }
